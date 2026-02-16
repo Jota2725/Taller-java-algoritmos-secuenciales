aa019084a19b61cd78af22bd3a863e8a928408fb
Julian Tirado <juliantirado71@gmail.com>
2026-02-16 01:00:08 -0500

    Punto4 solved

---
+++ Punto4.java
@@ -0,0 +1,12 @@
+
+public class Punto4 {
+    public static void main(String[] args) {
+
+        int resultado = Math.abs(-15);
+
+        System.out.println("El valor absoluto del número es: " + resultado);
+
+
+    }
+    
+}
