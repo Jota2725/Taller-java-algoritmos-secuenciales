b6bbbfd2b5287725727f3f4c0280eeb4ff0ff90d
Julian Tirado <juliantirado71@gmail.com>
2026-02-16 00:51:25 -0500

    Punto3 solved

---
+++ Punto3.java
@@ -0,0 +1,18 @@
+public class Punto3 {
+    public static void main(String[] args) {
+
+
+        double cantidadPesos = 13800;
+        double precioDolar = 3600;
+
+        double dolares = cantidadPesos / precioDolar;
+
+        System.out.println("Usted tiene " + dolares + " Dólares.");
+        
+
+       
+
+
+    }
+    
+}
