924a25a9f2f357fbc32617d7bbd532652dd2054e
Julian Tirado <juliantirado71@gmail.com>
2026-02-18 23:49:18 -0500

    Punto5 Solved

---
+++ Punto5.java
@@ -0,0 +1,17 @@
+public class Punto5 {
+    public static void main(String[] args) {
+        double volumen = 25.0;
+        double temperatura = 70;
+        double presion = 2.5;
+        double masa = (presion * volumen) / (0.37 * (temperatura + 460));
+
+        System.out.println("La masa obtenida es: " + masa);
+
+        
+
+
+        
+
+        
+    }
+}
