303850c27000a95179d5229b0f9f61ee6ab6925f
Julian Tirado <juliantirado71@gmail.com>
2026-02-19 00:13:08 -0500

    Punto 7 solved

---
+++ Punto7.java
@@ -0,0 +1,9 @@
+public class Punto7 {
+    public static void main(String[] args) {
+        int salarioAnterior = 1423500;
+        double nuevoSalario = salarioAnterior * 1.25;
+
+        System.out.println("El salario aumentó un 25%, este era el antiguo salario: " + salarioAnterior + ", el nuevo salario será de: " + nuevoSalario);
+    }
+    
+}
