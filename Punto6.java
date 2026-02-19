3fc2e02e91da87d4d87e1e56475a3becb87e1921
Julian Tirado <juliantirado71@gmail.com>
2026-02-18 23:56:26 -0500

    Punto 6 Solved

---
+++ Punto6.java
@@ -0,0 +1,10 @@
+public class Punto6 {
+    public static void main(String[] args)
+     {
+        int edad = 21;
+        int numPulsaciones = (220 - edad) / 10;
+
+        System.out.println("El número de pulsaciones por 10 segundos de ejercicio es: " + numPulsaciones);
+        
+            }
+}
