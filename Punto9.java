b588d06d6b615757bd1e2353b4c04786d01fc1fe
Julian Tirado <juliantirado71@gmail.com>
2026-02-19 16:37:16 -0500

    Punto 9 solved

---
+++ Punto9.java
@@ -0,0 +1,12 @@
+public class Punto9 {
+    public static void main(String[] args) {
+        int precioProducto = 500000;
+        double precioSugerido = precioProducto * 1.3;
+
+        System.out.println("El precio original del producto es de: " + precioProducto + 
+        "\n y para obtener ganancia del 30%, lo debe vender a: " + precioSugerido
+    );
+    }
+    
+    
+}
