094137a52be10ace0684f60dcb2cfa615b75ccc0
Julian Tirado <juliantirado71@gmail.com>
2026-02-19 16:26:03 -0500

    Punto 8 solved

---
+++ Punto8.java
@@ -0,0 +1,24 @@
+public class Punto8 {
+    public static void main(String[] args) {
+
+        int presupuestoAnual = 100000000;
+        
+        double ginecologia = presupuestoAnual * 0.40;
+        double traumatologia = presupuestoAnual * 0.30;
+        double pediatria = presupuestoAnual * 0.30;
+
+        System.out.println("El presupuesto anual del hospital es de: " + presupuestoAnual + ", y se repartirá de la siguiente manera:\n" +
+        "El presupuesto para ginecología es de: " + ginecologia +
+        "\nEl presupuesto para traumatología es de: " + traumatologia +
+        "\nEl presupuesto para pediatría es de: " + pediatria
+        
+         
+            
+        );
+         
+
+
+
+    }
+    
+}
